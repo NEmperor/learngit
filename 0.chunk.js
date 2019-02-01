386a92446d36f5b1f19eec706c8069aa7a185c56
NightEmperor <772052446@qq.com>
2019-02-01 14:20:45 +0800

    webpack

---
+++ 0.chunk.js
@@ -0,0 +1,21 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],{
+
+/***/ "./src/a.js":
+/*!******************!*\
+  !*** ./src/a.js ***!
+  \******************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var A = {
+  name: "JacK",
+  age: 18
+};
+/* harmony default export */ __webpack_exports__["default"] = (A);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=0.chunk.js.map
---
+++ 1.chunk.js
@@ -0,0 +1,21 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[1],{
+
+/***/ "./src/b.js":
+/*!******************!*\
+  !*** ./src/b.js ***!
+  \******************/
+/*! exports provided: B */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "B", function() { return B; });
+var B = {
+  name: "Tom",
+  age: 18
+};
+
+/***/ })
+
+}]);
+//# sourceMappingURL=1.chunk.js.map
